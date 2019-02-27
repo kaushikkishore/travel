b3063ce8ff073d9c18f9a68c1ae5e507bacdc0fd
Kaushik Kishore <Kaushik.Kishore@wdc.com>
2019-02-27 16:05:53 +0530

    implementing company header nav bar

--- src/component/comapny-header.jsx
+++ src/component/comapny-header.jsx
@@ -4,44 +4,44 @@ class CompanyHeader extends Component {
   state = {};
   render() {
     return (
-      <nav className="navbar navbar-expand-lg navbar-light bg-light">
-        <a className="navbar-brand" href="#">
-          Navbar w/ text
-        </a>
-        <button
-          className="navbar-toggler"
-          type="button"
-          data-toggle="collapse"
-          data-target="#navbarText"
-          aria-controls="navbarText"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span className="navbar-toggler-icon" />
-        </button>
-        <div className="collapse navbar-collapse" id="navbarText">
-          <ul className="navbar-nav mr-auto">
-            <li className="nav-item active">
-              <a className="nav-link" href="#">
-                Home <span className="sr-only">(current)</span>
-              </a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="#">
-                Features
-              </a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="#">
-                Pricing
-              </a>
-            </li>
-          </ul>
-          <span className="navbar-text">
-            Navbar text with an inline element
-          </span>
-        </div>
-      </nav>
+      <React.Fragment>
+        <nav className="navbar navbar-light bg-light">
+          <a className="navbar-brand">
+            <button
+              className="btn btn-outline-primary my-2 my-sm-0"
+              type="submit"
+            >
+              Application Logo goes here
+            </button>
+          </a>
+
+          <form className="form-inline">
+            <ul className="nav justify-content-end">
+              <li className="nav-item">
+                <a className="nav-link active" href="#">
+                  Active
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">
+                  Link
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">
+                  Link
+                </a>
+              </li>
+            </ul>
+            <button
+              className="btn btn-outline-success my-2 my-sm-0"
+              type="submit"
+            >
+              Make an Enquiry
+            </button>
+          </form>
+        </nav>
+      </React.Fragment>
     );
   }
 }
