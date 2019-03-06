7e3e8b1d4b946edcb8fd865006296ee8fdb1c8f6
Kaushik Kishore <Kaushik.Kishore@wdc.com>
2019-03-06 15:53:45 +0530

    Application details UI enhancement

---
+++ src/db/carousel-db.js
@@ -0,0 +1,23 @@
+export const CarouselSource = [
+  {
+    id: 1,
+    src: "/img/a.png",
+    alt: "First slide",
+    heading: "ITALY",
+    description: "Nulla vitae elit libero, a pharetra augue mollis interdum."
+  },
+  {
+    id: 2,
+    src: "/img/b.png",
+    alt: "Second slide",
+    heading: "ROME",
+    description: "Nulla vitae elit libero, a pharetra augue mollis interdum."
+  },
+  {
+    id: 3,
+    src: "/img/c.png",
+    alt: "Third slide",
+    heading: "INDIA",
+    description: "Nulla vitae elit libero, a pharetra augue mollis interdum."
+  }
+];
--- src/db/why_us.js
+++ src/db/why_us.js
@@ -1,15 +1,18 @@
 export const whyUsDB = [
   {
+    Id: 1,
     Heading: "EXPERTS IN EUROPEAN TOURS",
     Content:
       "Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature"
   },
   {
+    Id: 2,
     Heading: "HONEST AND RELAIABLE TEAM",
     Content:
       "Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature"
   },
   {
+    Id: 3,
     Heading: "LATEST TECH TO DELIVER TOP QUALITY SERVICES",
     Content:
       "Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature"
